1ac4df9f260ae84443472447cce97853bc76a3a1
Mark <mark@iMac2.local>
2018-09-14 13:57:49 -0400

    finished project

--- assignment_one.py
+++ assignment_one.py
@@ -1,11 +1,13 @@
+# this program is to draw a face with 2 eyes a nose and a mouth
+
 import turtle
 
-turtle.speed(20)
+turtle.speed(40)
 
 turtle.pencolor("yellow")
 
 turtle.color("yellow")
-#this is to fill in the face yellow
+# this is to draw the shape of the face
 turtle.begin_fill()
 
 turtle.circle(200)
@@ -14,35 +16,38 @@ turtle.end_fill()
 
 turtle.penup()
 
-turtle.goto(100,100)
+turtle.goto(100, 100)
 
 turtle.pencolor("black")
 
 turtle.pendown()
-
+# this part is to draw the mouth
 turtle.width(5)
 
 turtle.back(200)
 
 turtle.penup()
 
-turtle.goto(0,200)
+turtle.goto(0, 200)
 
 turtle.pendown()
 
-#this was to create a loop
-def drawANose():
+
+# this was to create a loop for the nose
+def draw_nose():
     for x in range(1):
         turtle.right(60)
         turtle.forward(75)
         turtle.right(130)
         turtle.forward(50)
-for x in range(1):
-    drawANose()
+
+
+for v in range(1):
+    draw_nose()
 
 turtle.penup()
 
-turtle.goto(100,250)
+turtle.goto(100, 250)
 
 turtle.color("black")
 
@@ -52,24 +57,88 @@ turtle.pendown()
 
 turtle.pencolor("black")
 
-#this part is to create a loop to draw an eye
-def drawAnEye():
-    for x in range(6):
+
+# this part is to create a loop to draw an eye
+def draw_eye():
+    for w in range(6):
         turtle.forward(30)
         turtle.left(60)
-for x in range(1):
+
+
+for z in range(1):
     turtle.color("black")
     turtle.begin_fill()
-    drawAnEye()
+    draw_eye()
     turtle.end_fill()
 
 turtle.penup()
-turtle.goto(-50,250)
+turtle.goto(-50, 250)
 turtle.pendown()
-for x in range(1):
+for y in range(1):
     turtle.color("black")
     turtle.begin_fill()
-    drawAnEye()
+    draw_eye()
     turtle.end_fill()
+
+# all the lines below are for drawing the hair
+turtle.penup()
+
+turtle.goto(165, 320)
+
+turtle.pendown()
+
+turtle.width(10)
+
+turtle.pencolor("brown")
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(120, 360)
+
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(70, 390)
+
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(10, 400)
+
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(-50, 390)
+
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(-120, 360)
+
+turtle.pendown()
+
+turtle.forward(50)
+
+turtle.penup()
+
+turtle.goto(-165, 320)
+
+turtle.pendown()
+
+turtle.forward(50)
 
 turtle.exitonclick()
